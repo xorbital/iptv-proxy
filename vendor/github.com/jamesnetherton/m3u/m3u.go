a3a1bc4e448eec57a9f8fca145d7c8e0546ccb0a
xorbital <53773184+xorbital@users.noreply.github.com>
2021-04-30 22:43:18 +0200

    Update m3u.go

--- vendor/github.com/jamesnetherton/m3u/m3u.go
+++ vendor/github.com/jamesnetherton/m3u/m3u.go
@@ -2,10 +2,14 @@ package m3u
 
 import (
 	"bufio"
+	"bytes"
 	"errors"
+	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -16,7 +20,7 @@ type Playlist struct {
 
 // A Tag is a simple key/value pair
 type Tag struct {
-	Name string
+	Name  string
 	Value string
 }
 
@@ -38,17 +42,18 @@ func Parse(fileName string) (playlist Playlist, err error) {
 	} else {
 		f, err = os.Open(fileName)
 	}
-	
+
 	if err != nil {
 		err = errors.New("Unable to open playlist file")
 		return
 	}
 	defer f.Close()
 	scanner := bufio.NewScanner(f)
+	tagsRegExp, _ := regexp.Compile("([a-zA-Z0-9-]+?)=\"([^\"]+)\"")
+
 	for scanner.Scan() {
 		line := scanner.Text()
-
-	if strings.HasPrefix(line, "#") || line == "" {
+		 if strings.HasPrefix(line, "#") || line == "" {
 			continue
 		} else if len(playlist.Tracks) == 0 {
 			err = errors.New("URI provided for playlist with no tracks")
@@ -59,4 +64,36 @@ func Parse(fileName string) (playlist Playlist, err error) {
 	}
 
 	return playlist, nil
+}
+
+// Marshall Playlist to an m3u file.
+func Marshall(p Playlist) (io.Reader, error) {
+	buf := new(bytes.Buffer)
+	w := bufio.NewWriter(buf)
+	if err := MarshallInto(p, w); err != nil {
+		return nil, err
+	}
+
+	return buf, nil
+}
+
+// MarshallInto a *bufio.Writer a Playlist.
+func MarshallInto(p Playlist, into *bufio.Writer) error {
+	into.WriteString("#EXTM3U\n")
+	for _, track := range p.Tracks {
+		into.WriteString("#EXTINF:")
+		into.WriteString(fmt.Sprintf("%d ", track.Length))
+		for i := range track.Tags {
+			if i == len(track.Tags)-1 {
+				into.WriteString(fmt.Sprintf("%s=%q", track.Tags[i].Name, track.Tags[i].Value))
+				continue
+			}
+			into.WriteString(fmt.Sprintf("%s=%q ", track.Tags[i].Name, track.Tags[i].Value))
+		}
+		into.WriteString(", ")
+
+		into.WriteString(fmt.Sprintf("%s\n%s\n", track.Name, track.URI))
+	}
+
+	return into.Flush()
 }
